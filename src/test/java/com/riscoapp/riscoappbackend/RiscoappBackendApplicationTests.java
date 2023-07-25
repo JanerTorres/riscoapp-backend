9a216ebb62538ea44ad0f990e8a95e9d0574c09a
janertorres <janer.torres@udea.edu.co>
2023-07-25 13:40:13 -0500

    Configuración de proyecto backend en Spring boot

---
+++ src/test/java/com/riscoapp/riscoappbackend/RiscoappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riscoapp.riscoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiscoappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
